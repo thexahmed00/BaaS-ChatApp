bf6883e5cd8fb126d89f4bac833b1b10a531481c
thexahmed00 <ma2k510@gmail.com>
2023-08-26 12:20:37 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Room from './pages/room' 
+
+function App() {
+  
+  return (
+    <>
+      <Room/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/room.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Room = () => {
+  return (
+    <div>hello chat</div>
+  )
+}
+
+export default Room
