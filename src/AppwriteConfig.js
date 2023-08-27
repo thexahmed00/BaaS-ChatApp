10093f182cebf4576ca1832391b118864bf46620
thexahmed00 <ma2k510@gmail.com>
2023-08-27 21:57:09 +0530

    connected to appwrite

---
+++ src/AppwriteConfig.js
@@ -0,0 +1,30 @@
+import { Client,Databases,Account} from 'appwrite';
+
+export const PROJECT_ID = '64e99e2a6f083eaca059';
+export const DATABASE_ID = '64e9a3b43d45e4738335';
+export const COLLECTION_ID = '64e9a3bc9f2757d56258';
+
+const appwrite = new Client();
+
+
+
+appwrite
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('64e99e2a6f083eaca059');
+
+//     const account = new Account(appwrite);
+
+// // Register User
+// account.create(
+//     'me@example.com',
+//     'password',
+//     'Jane Doe'
+// ).then(response => {
+//     console.log(response);
+// }, error => {
+//     console.log(error);
+// });
+
+export const databases= new Databases(appwrite);
+
+export default appwrite;
--- src/pages/room.jsx
+++ src/pages/room.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { COLLECTION_ID, DATABASE_ID, databases } from '../AppwriteConfig'
 
 const Room = () => {
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    getmessages()
+  }, []) 
+
+  const getmessages = async () => {
+    const messages = await databases.listDocuments(DATABASE_ID, COLLECTION_ID)
+    console.log("Response: ",messages)
+    setMessages(messages.documents)
+  }
   return (
-    <div>hello chat</div>
+    <div>
+      {messages.map(message => (
+        <div key={message.$id}>
+          
+          <div>{message.$createdAt}</div>
+
+          <div>
+            <span>{message.body}</span>
+          </div>
+        </div>
+      ))}
+    </div>
   )
+  
 }
 
 export default Room
