71ca36d4a4234e5b56e57bc8f23b28ea918108c1
Affanzuhaib <affanzuhaibuddin@gmail.com>
2023-10-26 17:18:36 +0530

    Updated the UI/UX of login page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -19,43 +19,44 @@ const RegisterPage = () => {
     }
 
   return (
-    <div className="auth--container">
-        <div className="form--wrapper">
-            <form onSubmit={(e)=>{handleRegister(e,credentials)}}>
-            <div className="field--wrapper">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" required name="name" 
+    <div className="Auth-form-container">
+        <form className='Auth-form-register' onSubmit={(e)=>{handleRegister(e,credentials)}}>
+        <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign Up</h3>
+            <div className="form-group mt-3">
+                    <label className="label" htmlFor="name">Name</label>
+                    <input className="form-control mt-1" type="text" required name="name" 
                                 placeholder="Enter your Name.."
                                 value={credentials.name}
                                 onChange={handleInput}/>
                 </div>
-                <div className="field--wrapper">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" required name="email" 
+                <div className="form-group mt-3">
+                    <label className="label" htmlFor="email">Email</label>
+                    <input className="form-control mt-1" type="email" required name="email" 
                                 placeholder="enter your email.."
                                 value={credentials.email}
                                 onChange={handleInput}/>
                 </div>
-                <div className="field--wrapper">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" required name="password" 
+                <div className="form-group mt-3">
+                    <label className="label" htmlFor="password">Password</label>
+                    <input className="form-control mt-1" type="password" required name="password" 
                                 placeholder="Enter your password.."
                                 value={credentials.password}
                                 onChange={handleInput}/>
                 </div>
-                <div className="field--wrapper">
-                    <label htmlFor="password">Confirm password</label>
-                    <input type="password" required name="password2" 
+                <div className="form-group mt-3">
+                    <label className="label" htmlFor="password">Confirm password</label>
+                    <input className="form-control mt-1" type="password" required name="password2" 
                                 placeholder="Confirm your password.."
                                 value={credentials.password2}
                                 onChange={handleInput}/>
                 </div>
-                <div className="field--wrapper">
-                    <input type="submit" value="Register" className="btn btn--lg btn--main"/>
+                <div className="Auth--field">
+                    <input type="submit" value="Sign Up" className="Auth--field--input"/>
                 </div>
-       <p> <Link to="/login" className="link--secondary">Already have an account? Login</Link></p>
-            </form>
+       <p className="Forgot--password"> <Link to="/login" className="link--secondary">Already have an account? Login</Link></p>
         </div>    
+        </form>
     </div>
   )
 }
